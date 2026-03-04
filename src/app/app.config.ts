59e57ac3fb29f4e1a937f68f9628a0fac4ee3bf9
ninjasrj <ninjagjm@gmail.com>
2026-03-04 12:10:20 +0600

    I don't know what I am doing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideReactiveForms } from '@angular/forms'; // Import the provider
 
 import { routes } from './app.routes';
 
@@ -9,6 +8,5 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideReactiveForms() // Add this function to the providers array
   ]
 };
--- src/app/contract-form/contract-form.ts
+++ src/app/contract-form/contract-form.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Import Angular Material modules for the UI
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-contract-form',
-  imports: [],
-  templateUrl: './contract-form.html',
-  styleUrl: './contract-form.scss',
+  standalone: true, // This is key for Angular 20!
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatRadioModule
+  ],
+  templateUrl: './contract-form.component.html',
+  styleUrls: ['./contract-form.component.scss']
 })
-export class ContractForm {
+export class ContractFormComponent implements OnInit {
+  // We divide the form into Steps
+  generalInfoStep!: FormGroup;
+  stakeholderStep!: FormGroup;
+  inspectionStep!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    // Logic for Step 1: General Info
+    this.generalInfoStep = this.fb.group({
+      // We use a "Pattern" validator for your long contract numbers
+      contractNo: ['', [Validators.required, Validators.pattern(/^[0-9.]+$/)]],
+      shortNo: ['', Validators.required],
+      fiscalYear: ['2024-25', Validators.required],
+      contractDate: [null, Validators.required],
+      itemType: ['RawMat', Validators.required],
+      contractName: ['', Validators.required],
+      pdsDate: [null],
+      userBr: ['', Validators.required]
+    });
+
+    // Step 2 & 3 will be initialized here later...
+  }
 }
